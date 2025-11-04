25357d9547479f71e989130453c9c964832fd8a5
manual2011 <reyesmanuelzarate@gmail.com>
2025-11-05 00:50:46 +0100

    original

--- src/config.js
+++ src/config.js
@@ -1,5 +1,12 @@
-export let PORT = process.env.PORT || 3000;
+import { config} from "dotenv";
 
-export let DB_USER = process.env.MYSQLUSER || "root"; export let DB_HOST = process.env.MYSQLHOST || "localhost"; export let DB_PASSWORD = process.env.MYSQLPASSWORD || "root"; export let DB_PORT = process.env.MYSQLPORT || 3306;
+config()
 
-export let DB_DATABASE = "empresas_telefonia";
+export let PORT=process.env.PORT||3000
+export let DB_USER=process.env.DB_USER||"root"
+export let DB_HOST=process.env.DB_HOST||"localhost"
+export let DB_DATABASE=process.env.DB_DATABASE||"empresas_telefonia"   
+export let DB_PASSWORD=process.env.DB_PASSWORD||"root"
+export let DB_PORT=process.env.DB_PORT||3306
+
+
