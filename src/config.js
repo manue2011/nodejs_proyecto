5942afe98ca043200221656f8d27951fa1af06e4
manual2011 <reyesmanuelzarate@gmail.com>
2025-11-05 00:34:53 +0100

    nueva solucion

--- src/config.js
+++ src/config.js
@@ -1,12 +1,13 @@
-import { config} from "dotenv";
+export let PORT = process.env.PORT || 3000;
 
-config()
+// --- AQUI ESTÁ LA MAGIA (El "Traductor") ---
+export let DB_USER = process.env.MYSQLUSER || "root";
+export let DB_HOST = process.env.MYSQLHOST || "localhost";
+export let DB_PASSWORD = process.env.MYSQLPASSWORD || "root";
+export let DB_PORT = process.env.MYSQLPORT || 3306;
 
-export let PORT=process.env.PORT||3000
-export let DB_USER=process.env.DB_USER||"root"
-export let DB_HOST=process.env.DB_HOST||"localhost"
-export let DB_DATABASE=process.env.DB_DATABASE||"empresas_telefonia"   
-export let DB_PASSWORD=process.env.DB_PASSWORD||"root"
-export let DB_PORT=process.env.DB_PORT||3306
-
-
+// --- ¡¡ESTA ES LA LÍNEA MÁS IMPORTANTE!! ---
+// Forzamos a la app a usar la base de datos que TÚ creaste en DBeaver,
+// ignorando la que da Railway por defecto ("railway").
+// (Asumo que el nombre no tiene espacios).
+export let DB_DATABASE = "empresas_telefonia";
