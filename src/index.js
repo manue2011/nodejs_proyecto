d12de88a087bcab9ed47b633bf9b6d74556393e8
manual2011 <reyesmanuelzarate@gmail.com>
2025-11-05 01:29:18 +0100

    coneccion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import app from "./app.js"
-import {PORT} from "./config.js"
- 
+import { PORT } from "./config.js"
+import { pool } from "./db.js" // importa tu pool
+
+// Test de conexión MySQL al iniciar el servidor
+(async () => {
+  try {
+    await pool.query("SELECT 1");
+    console.log("Conexión exitosa a MySQL 🚀");
+  } catch (error) {
+    console.error("Error al conectar a MySQL:", error);
+  }
+})();
+
 app.listen(PORT)
-console.log("server is running on port ",PORT)
-
-
+console.log("server is running on port ", PORT)
